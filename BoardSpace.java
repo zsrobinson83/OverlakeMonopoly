2d7e564ac37288dfb9f73dc7a991a0f78e8a7a60
MistHelix <88747182+MistHelix@users.noreply.github.com>
2022-05-17 09:18:26 -0700

    fixed player

--- BoardSpace.java
+++ BoardSpace.java
@@ -6,13 +6,18 @@ public class BoardSpace {
     private int tax; 
     private String type;
     private boolean owned = false;
+    private String color;
 //We can deterimne which side of the board it is on by dividing it by 4.
-    public BoardSpace(int location,int value,int houses,int hotels, int tax, String type){
+    public BoardSpace(int location,int value,int houses,int hotels, int tax, String type, String Color){
         this.location = location;
         this.value = value;
         this.houses = houses;
         this.hotels = hotels;
         this.tax = tax;
-        this.type = type;
+        this.type = type
+        this.color = color;
     } 
+    public String getName(){
+        return 
+    }
 }
