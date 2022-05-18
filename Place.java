3accc313ec12c6e602850bff4995a9697f11c94b
MistHelix <88747182+MistHelix@users.noreply.github.com>
2022-05-18 10:41:28 -0700

    E block work pt2

--- Place.java
+++ Place.java
@@ -1,23 +1,45 @@
-public class BoardSpace {
-    private int location;
-    private int value;
-    private int houses;
-    private int hotels;
-    private int tax; 
-    private String type;
-    private boolean owned = false;
-    private String color;
-//We can deterimne which side of the board it is on by dividing it by 4.
-    public BoardSpace(int location,int value,int houses,int hotels, int tax, String type, String Color){
-        this.location = location;
-        this.value = value;
-        this.houses = houses;
-        this.hotels = hotels;
-        this.tax = tax;
-        this.type = type
-        this.color = color;
-    } 
-    public String getName(){
-        return 
+
+//Final Project
+//Alec Zoe Felix Celina Gavin
+//Overlake Monopoly
+//place class creates the place objects of the game
+import java.util.*;
+public class Place{
+
+    private Player owner;
+    private int cost;
+    private int rent;
+   
+    public Place(Player person, int costs, int rents){
+   
+       owner = person;
+       cost = costs;
+       rent = rents;
+   
     }
-}
+   //What int are we returning from this? Because this should probably be a void method.
+    public int runPlace(Player play){
+   
+ 
+       if(this.owner ==  null){
+              System.out.println("Do you want to buy this property? Cost to buy: " + cost);
+              Scanner myObj = new Scanner(System.in);
+              String response = myObj.nextLine();
+              if(response.toLowerCase().equals("yes")){
+              play.take(cost);
+             this.owner = play;
+              }
+       }
+       else if(this.owner == play){
+ 
+       }
+       else{
+ play.take(rent);
+ owner.give(rent);
+       }
+       
+   
+    }
+ 
+ }
+ 
--- Player.java
+++ Player.java
@@ -1,5 +1,60 @@
+//Final Project
+//Alec Zoe Felix Celina Gavin
+//Overlake Monopoly
+//Player class Creates players
 import java.util.*;
-public class Player {
-    private int money = 1500;
-    private ArrayList<BoardSpace> owned = new ArrayList<BoardSpace>();
-}
+public class Player{
+
+    private int bank;
+    private int pos;
+    private ArrayList<Place> owner = new ArrayList<Place>();
+   
+    public Player(){
+   
+       bank = 1500;
+       pos = 0;
+   
+    }
+   
+    public void give(int num){
+   
+       bank += num;
+   
+    }
+   
+    public void take(int num){
+   
+       bank -= num;
+   
+    }
+   
+    public void move(int num){
+   
+       for(int i = num; i > 0; i--){
+          if(pos == 3){
+             pos = 0;
+          }
+          else{
+             pos++;
+          }
+            
+       }
+   
+    }
+ 
+    public void ownPlace(){
+    }
+   
+    public int getBank(){
+       return bank;
+    }
+    public int getPos(){
+       return pos;
+    }
+   
+   
+   
+ 
+ 
+ }
+ 
