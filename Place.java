599884155baabaa7bde2a8b9514745753e776ea8
Alec-Odell <89097429+Alec-Odell@users.noreply.github.com>
2022-05-20 12:10:48 -0700

    Update Place.java

--- Place.java
+++ Place.java
@@ -10,16 +10,22 @@ public class Place{
    private int cost;
    private int rent;
    private String name;
+   private boolean isPlace;
+   private boolean isCard;
    private boolean finished = false;
-    public Place(String placeName ,Player person, int costs, int rents){
+   public Place(String placeName ,Player person, int costs, int rents, boolean place, boolean card){
       owner = person;
       cost = costs;
       rent = rents;
       name = placeName;
+      isPlace = place;
+      isCard = card;
+      
     }
-   //What int are we returning from this? Because this should probably be a void method.
+   
     public void runPlace(Player play){
-      if(this.owner ==  null){
+       if(this.isPlace){
+         if(this.owner ==  null){
               System.out.println("Do you want to buy this property? Cost to buy: " + cost);
               Scanner myObj = new Scanner(System.in);
               String response = myObj.nextLine();
@@ -32,23 +38,32 @@ public class Place{
                   }
                   else if (response.toLowerCase().equals("no")){
                      //auction or does nothing
+                     //I dont think auctions are the best idea, maybe we can add it later
                   }
                   else {
                      System.out.println("Not a valid response");
                   }
               }
                
-            }
-      else if(this.owner == play){
+         }
+         else if(this.owner == play){
  
-      }
-      else{
- play.take(rent);
- owner.give(rent);
+         }
+         else{
+            play.take(rent);
+            owner.give(rent);
+         }
        }
-       
    
-    }
+       if(this.isCard){
+         randNum = (int)(Math.random() * 3)
+         if(randNum == 0){
+            System.out.println("You fail ap calc, and lose 50 dollars. ");
+            play.take(50);
+         }
+      }
+   
+   
    public String getName(){
       return this.name;
    }
